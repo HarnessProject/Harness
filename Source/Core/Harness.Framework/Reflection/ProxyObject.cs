9370a303d08fb5da299a75daefc73add41403b03
Nick Daniels <nick@fndsnd.com>
2014-05-03 19:32:29 +0000

    More Changes for Aspects

---
+++ Source/Core/Harness.Framework/Reflection/ProxyObject.cs
@@ -0,0 +1,25 @@
+﻿using Dynamitey;
+using Dynamitey.DynamicObjects;
+using Harness.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Harness.Framework.Reflection
+{
+
+    public class ProxyObject(object obj) : BaseForwarder(obj)
+    {
+
+    }
+
+    public static class ReflectionExtensions
+    {
+        public static T ActLike<T>(this object target, params Type[] types)
+        {
+            var proxy = new Get(target);
+            
+    }
+}
--- Source/Harness.Aspects/Class1.cs
+++ Source/Harness.Aspects/Class1.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Dynamitey;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Harness.Aspects
 {
-    public class Class1
-    {
-    }
+   
 }
